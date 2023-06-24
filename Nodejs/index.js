59ee030a46077f3d9b1119d32b2d2de444b163c8
jjPark9 <lucero23@naver.com>
2023-06-24 15:01:19 +0900

    Install lodash

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,1 +1,9 @@
-console.log('index run');
+import circle from './circle.js';
+import lodash from 'lodash';
+
+console.log(circle.PI, circle.getArea(1), circle.getCircumference(1));
+
+const arr = [1, 1, 1, 2, 2, 1, 1, 4, 4, 3, 2];
+const uniqueArr = lodash.uniqBy(arr);
+
+console.log(uniqueArr);
