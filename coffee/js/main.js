66ee3bd6d7bed04489f10aad491201fbc2be7c6e
jjPark9 <lucero23@naver.com>
2023-06-26 14:40:24 +0900

    Global Badge

--- coffee/js/main.js
+++ coffee/js/main.js
@@ -16,3 +16,20 @@ searchInputEl.addEventListener('blur', () => {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', () => {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    gsap.to(badgeEl, 0.6, {
+      opacity: 0,
+      display: 'none',
+    });
+  } else {
+    gsap.to(badgeEl, 0.6, {
+      opacity: 1,
+      display: 'block',
+    });
+  }
+});
